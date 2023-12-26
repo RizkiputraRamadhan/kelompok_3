8fab9938c01cc57584c41ab97ee07e4740a26f79
YusufKamaludinRomadhon <yusufkamaludin175@gmail.com>
2023-12-26 20:57:22 +0700

    controller admin

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -103,7 +103,90 @@ class DashboardController extends Controller
         ]);
         return redirect('/admin/room')->with('success', 'Data Room Successfully !!');
     }
+    public function adminEdit($id)
+    {
+        $room = Room::find($id);
 
+        if ($room) {
+            return view('v_admin.edit', ['room' => $room]);
+        } else {
+            return redirect('/admin/room')->with('error', 'Room not found.');
+        }
+    }
+    public function adminUpdate(Request $request, $id)
+    {
+        $validatedData = $request->validate([
+            'nama_kamar' => 'required|string',
+            'harga' => 'required|numeric',
+            'peta' => 'required|string',
+            'no_kamar' => 'required|string',
+            'kapasitas' => 'required|string',
+            'category_pinjam' => 'required|string',
+            'category' => 'required|string',
+            'file' => 'image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+            'desc' => 'required|string',
+            'status' => 'required|string',
+        ]);
+
+        $room = DB::table('room')
+            ->where('id', $id)
+            ->first();
+
+        if (!$room) {
+            return redirect('/admin/room')->with('error', 'Room not found.');
+        }
+
+        $imagePath = $room->image;
+
+        if ($request->hasFile('file')) {
+            $file = $request->file('file');
+            $fileName = time() . '.' . $file->getClientOriginalExtension();
+            $filePath = $file->storeAs('public', $fileName);
+            $imagePath = asset('storage/' . $fileName);
+        }
+
+        DB::table('room')
+            ->where('id', $id)
+            ->update([
+                'no_kamar' => $validatedData['no_kamar'],
+                'kapasitas' => $validatedData['kapasitas'],
+                'category_pinjam' => $validatedData['category_pinjam'],
+                'nama_kamar' => $validatedData['nama_kamar'],
+                'harga' => $validatedData['harga'],
+                'category' => $validatedData['category'],
+                'peta' => $validatedData['peta'],
+                'desc' => $validatedData['desc'],
+                'status' => $validatedData['status'],
+                'image' => $imagePath,
+                'updated_at' => now(),
+            ]);
+
+        return redirect('/admin/room')->with('success', 'Room updated successfully.');
+    }
+
+    public function scanQr($code_QR)
+    {
+        $room = Room::where('code_QR', $code_QR)->first();
+
+        if ($room) {
+            if ($room->user_id) {
+                $guest = Guest::where('user_id', $room->user_id)->first();
+                if (!$guest) {
+                    return redirect('/admin/home')->with('error', 'Tamu Belum terverifikasi.');
+                }
+            }
+            if ($room->status == 2) {
+                return redirect('/admin/home')->with('error', 'Room sudah dipesan.');
+            }
+            $room->update([
+                'status' => 2,
+            ]);
+
+            return redirect('/admin/home')->with('success', 'Scan successfully.');
+        }
+
+        return redirect('/admin/home')->with('error', 'Room tidak ditemukan.');
+    }
     public function destroy($id)
     {
         $room = Room::find($id);
